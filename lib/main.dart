9e15ec830a7ebb9730e8d2db805ab498771591b4
Miguel-Deza <dmikec101@gmail.com>
2023-01-25 11:54:20 -0500

    finish project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:quizzler_project_app/question.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -16,106 +22,91 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   List<Icon> scoreKeeper = [];
 
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true)
-  ];
-
-  int questionNumber = 0;
-
-  void increaseQuestion() {
-    if (questionNumber < questionBank.length - 1) {
-      questionNumber++;
-    }
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getQuestionAnswer();
+    setState(() {
+      if (quizBrain.isEndQuiz()) {
+        Alert(
+                context: context,
+                title: "Finished",
+                desc: "You finished the quiz")
+            .show();
+        quizBrain.setQuestionNumberreset();
+        scoreKeeper = [];
+      } else {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(
+            Icon(
+              Icons.close,
+              color: Colors.red,
+            ),
+          );
+        }
+        quizBrain.nextQuestion();
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Trivia Game"),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              flex: 3,
-              child: Center(
-                child: Text(
-                  questionBank[questionNumber].questionText,
-                  style: TextStyle(fontSize: 30),
-                  textAlign: TextAlign.center,
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Trivia Game"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 3,
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                style: TextStyle(fontSize: 30),
+                textAlign: TextAlign.center,
               ),
             ),
-            Row(
-              children: [
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      bool correctAnswer =
-                          questionBank[questionNumber].questionAnswer;
-                      if (correctAnswer == false) {
-                        print("user got it right!");
-                      } else {
-                        print('User got it wrong!');
-                      }
-                      // Don't forget to update the state with SetState
-                      setState(() {
-                        increaseQuestion();
-                        scoreKeeper.add(
-                          Icon(
-                            Icons.close,
-                            color: Colors.red,
-                          ),
-                        );
-                      });
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(30.0),
-                      child: Text(
-                        "False",
-                        style: TextStyle(color: Colors.red),
-                      ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    checkAnswer(false);
+
+                    // Don't forget to update the state with SetState
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Text(
+                      "False",
+                      style: TextStyle(color: Colors.red),
                     ),
                   ),
                 ),
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      bool correctAnswer =
-                          questionBank[questionNumber].questionAnswer;
-                      if (correctAnswer == true) {
-                        print("user got it right!");
-                      } else {
-                        print('User got it wrong!');
-                      }
-                      setState(() {
-                        increaseQuestion();
-                        scoreKeeper.add(Icon(
-                          Icons.check,
-                          color: Colors.green,
-                        ));
-                      });
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.all(30.0),
-                      child: Text(
-                        "True",
-                        style: TextStyle(color: Colors.green),
-                      ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    checkAnswer(true);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Text(
+                      "True",
+                      style: TextStyle(color: Colors.green),
                     ),
                   ),
                 ),
-              ],
-            ),
-            Row(children: scoreKeeper),
-          ],
-        ),
+              ),
+            ],
+          ),
+          Row(children: scoreKeeper),
+        ],
       ),
     );
   }
